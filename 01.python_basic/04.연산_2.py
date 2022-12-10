f7c352d28d4202634401dbaeb6ace79070b17133
J1W0N-1209 <nabook754@naver.com>
2022-12-10 19:12:12 +0900

    실습문제

---
+++ 01.python_basic/04.연산_2.py
@@ -0,0 +1,19 @@
+# 1. 비교연산
+print("- 비교연산 문제")
+print(2 > 3) # Fasle
+print(15 < 30) # True
+print(1.5 >= 0) # True
+print(3 <= 3) # True
+print("월천만원" == "월이천만원") # False
+print("연봉1억" != "연봉10억") # True
+
+# 2. 논리연산
+print("- 논리연산 문제")
+print(4 < 6 and 10 >= 10) # True and True -> True
+print("포기" != "포기" or "안해" == "안해") # Flase or True -> True
+print(not 5==5) # not True -> False
+
+# 3. 멤버십 연산
+print("- 멤버십 연산 문제")
+print("a" in "abc") # True
+print("d" not in "abc") # True
